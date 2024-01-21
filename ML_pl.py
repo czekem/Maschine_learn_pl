c5944e6e8c58190c89877690c27df9d9226a4b0a
Michael Czekaj <145675402+czekem@users.noreply.github.com>
2024-01-21 19:27:36 +0100

    Update ML_pl.py

--- ML_pl.py
+++ ML_pl.py
@@ -2,12 +2,14 @@
 # ( base on : https://sjp.pwn.pl/poradnia/haslo/frekwencja-liter-w-polskich-tekstach;7072.html)
 # a, i, o, e, z, n , r
 
-
+# most often verb 'end' in polish language:
+# base on ( https://depot.ceon.pl/bitstream/handle/123456789/20067/Czasownik_polski.pdf?sequence=6&isAllowed=y )
 
 
 class MyClass:
     
     letters = ['a','i','e','o','n','r']
+    letters_2 = ['ło', 'ć', 'ał', 'ała', 'iesz', 'dzie',]  
     def __init__(self, object: str):
         self.object = object
         self.new_list = []
@@ -22,14 +24,52 @@ class MyClass:
     def __repr__(self) -> str:
         return f"{self.object!r}"
 
-            
+    def check_if_verb2(self, letters_2):
+        if self.object[-1] in letters_2:
+            self.new_list.append(self.object)   
 
 lst = ['gas', 'oil', 'water', 'chem', 'ala', 'ela']
-my_obje = []
 
-for item in lst:
-    obj = MyClass(item)
-    obj.check_and_append(MyClass.letters)
-    print(obj)
-    my_obje.append(obj)
+
+# my_obje = []
+
+# for item in lst:
+#     obj = MyClass(item)
+#     obj.check_and_append(MyClass.letters)
+#     print(obj)
+#     my_obje.append(obj)
     
+    
+  
+    
+lst_2 = ['spała', 'biegał', 'latało', 'kichało', 'wylało', 'krowa', 'las', 'piękny',]
+
+
+def checking_for_verb(lst):
+    """
+    This function is checking if the choosen word is a verb
+    in a polish language.
+    """
+    letters_2 = ['ło', 'ć', 'ał', 'ała', 'iesz', 'dzie',]  
+    for item in lst:
+        for word in letters_2:
+            if item.endswith(word):
+                print(item)
+                
+
+checking_for_verb(lst_2)
+
+
+def check_for_polish_word(lst):
+    """
+    This function is checking if the choosen 
+    word is a polish(stil needed to rework on this part)
+    """
+    letters = ['a','i','e','o','n','r']
+    for item in lst:
+        for word in letters:
+            if item.startswith(word):
+                print(item)
+                
+                
+check_for_polish_word(lst) # need to work on it.
